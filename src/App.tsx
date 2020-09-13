e4f8b444a7b6b8b81cfc8566f8204474ffebd1bb
dmithamo <dennis@novek.io>
2020-09-13 09:26:54 +0300

    feat(init): intialize a react app and set up a dev env

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        width: '100%',
+      }}
+    >
+      <h2>Hae!</h2>
+      <p style={{ opacity: '0.4' }}>Coming soon</p>
+    </div>
+  );
+}
+
+export default App;
